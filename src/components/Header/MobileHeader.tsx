5137cf1c26c36a2577a632cd1f9f3f94a1c05519
Anil <khalifaanil84@gmail.com>
2024-07-28 09:20:12 +0545

    "some changes"

---
+++ src/components/Header/MobileHeader.tsx
@@ -0,0 +1,70 @@
+import { Link } from "react-router-dom";
+import Snippet from "../Documentation/Snippet";
+
+function MobileHeader(): JSX.Element {
+  const code = ` 
+     nikaal "Hello World";
+     rakh a = 3;
+     
+     yedi (a < 3){
+       nikaal "a chai 3 bhanda syano chha";
+     } navaye (a == 3){
+       nikaal "a chai 3 chha"; 
+     } haina bhane {
+       nikaal "a chai 3 bhanda thulo chha"; 
+     }
+    `;
+
+  return (
+    <div className="min-h-screen bg-bg flex flex-col md:flex-row w-full p-4 md:p-8">
+      {/* logo */}
+      <div className="w-full flex justify-center md:justify-start mb-6 mt-[2rem]">
+        <img
+          src="/assets/images/mobile.png"
+          alt="logo"
+          className="w-[8rem] h-auto object-contain"
+        />
+      </div>
+      {/* main heading  */}
+      <div className="w-full md:w-[70%] flex flex-col gap-y-6">
+        {/* menu bar  */}
+        <div className="flex justify-center md:justify-start gap-x-6 md:gap-x-10">
+          <Link to="/">
+            <span className="font-opensans font-bold cursor-pointer">Docs</span>
+          </Link>
+          <Link to="/playground">
+            <span className="font-opensans font-bold cursor-pointer">
+              Playground
+            </span>
+          </Link>
+        </div>
+        {/* main text  */}
+        <div className="flex flex-col md:flex-row w-full gap-6 md:gap-x-5">
+          <div className="w-full md:w-[50%] flex flex-col gap-y-4">
+            <span className="leading-relaxed tracking-wide text-center md:text-left">
+              NepaliLang (.NEP) is a toy programming language developed in
+              TypeScript. This is the first version, and it will be updated
+              regularly to incorporate new features and improvements.
+            </span>
+            <div className="my-2 flex justify-center md:justify-start">
+              <span className="py-2 px-2 rounded-md bg-[#1e1e1e] text-white font-opensans cursor-text break-all">
+                npm i nepalilang
+              </span>
+            </div>
+            <div className="mt-2 flex justify-center md:justify-start">
+              <span className="py-4 px-8 bg-green text-white font-bold rounded-md font-opensans cursor-pointer">
+                Try Now
+              </span>
+            </div>
+          </div>
+          {/* code snippet  */}
+          <div className="w-full md:w-[50%]">
+            <Snippet code={code} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MobileHeader;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,18 @@
 import Documentation from "../../components/Documentation";
 import Heading from "../../components/Header/Heading";
+import MobileHeader from "../../components/Header/MobileHeader";
 
 function Home(): JSX.Element {
   return (
     //full container
     <div className="w-screen h-[100%]">
       {/* heading container */}
-      <div className="md:h-[30rem]">
+      <div className="md:h-[30rem] hidden md:block">
         <Heading />
+      </div>
+      {/* for mobile */}
+      <div className=" md:hidden">
+        <MobileHeader/>
       </div>
       {/* Documentation  */}
       <div>
