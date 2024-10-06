92e2e95c9b636c625bccf060ee4f751014b93f84
Anil <khalifaanil84@gmail.com>
2024-10-06 20:00:27 +0545

    changes in footer

--- src/components/Header/Bottom.jsx
+++ src/components/Header/Bottom.jsx
@@ -5,7 +5,7 @@ function Bottom() {
     <div className=" flex items-center justify-center">
       <div className="w-[90%] flex flex-row">
         <span className="font-poppins">
-          @2021 NepaliLang. Created by{" "}
+          @2024 NepaliLang. Created by{" "}
           <a
             className="text-blue-500"
             href="https://www.linkedin.com/in/anilbhandari24/"
